0942e05f6103786017b678a724c395ae539f98e3
Alexandra Romanova <alexandraromanova@Alexandras-Air.home>
2019-06-09 08:25:39 -0400

    welcome  root stuff, preparation for pushing to Heroku

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,6 @@
+class WelcomeController < ApplicationController
+
+    def index
+        render json: { status: 200, message: "Art create API" }
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   resources :users, only: [:index, :show, :create, :destroy] do
   resources :arts
 end
